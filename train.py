3fee746f433344856b5754f26e0d9814f69bd5be
BrittX <brittxbarnes@gmail.com>
2017-04-02 12:23:15 -0700

    getSamples() works and started Algo
    Fixed the try/except statement so that it throws an error when user
    inputs something other than a readable txt file. Began writing the algo,
    it stores the pairs, inputs, and number of pairs correctly, but working
    on a good way to store weights of the training samples

--- train.py
+++ train.py
@@ -10,11 +10,30 @@ def getSamples():
 	print('\nPlease enter the name of your training samples file: ')
 	try:
 		train = input(">>> ")
-		if train.endswith('.txt') and os.path.getsize(train) > 0:
-			return train
+		# Try to open the file and store its contents
+		with open(train) as training:
+			contents = training.readlines()
+		hopAlgo(contents)
+		return contents
 	except FileNotFoundError:
 		print("Invalid filename, please try again")
 		getSamples()
-	except TypeError:
-		print("Please enter a string with the name of the text file")
-		getSamples()
+
+"""
+Function to read and store each line in training file
+"""
+def hopAlgo(tContents):
+	# Store contents as # inputs, training pairs
+	tpairs = []
+	for i, item in enumerate(tContents):
+		if i == 0: ins = int(item)
+		elif i == 1: pairs = int(item)
+		# Not include initial new line before training pairs
+		elif i == 2 and item == '\n': continue
+		else: tpairs.append(item)
+	weights = [[0 for x in range(ins)] for y in range(pairs)]
+	for row in tpairs:
+		for i, item in enumerate(row):
+			if item == ' ': weights[i] = -1
+			if item != ' ': weights[i] = 1
+	print(weights)
